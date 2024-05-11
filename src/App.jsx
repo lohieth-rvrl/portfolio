952afeb767a03b3e3e8e89b6b8621480b1961586
lohieth <loheithrvrl121@gmail.com>
2024-05-11 18:07:05 +0530

    modified 2

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ export default function App() {
       <Exp /> 
       <Contact/>
       <All/>
-      <ConForm />
     </div>
   );
 }
--- src/Components/ConFom.jsx
+++ src/Components/ConFom.jsx
@@ -1,34 +1,85 @@
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 function ConForm() {
   const [state, handleSubmit] = useForm("mrgndbwl");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return <p>Thanks for joining!</p>;
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
+    <form
+      onSubmit={handleSubmit}
+      action="#"
+      class=" p-md-5 contact-form"
+      id="contact-form"
+    >
+      <div class="row">
+        <div class="col-md-12 d-flex justify-content-between">
+          <div class="col-md-5 form-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Your Name"
+              fdprocessedid="mpdmc9"
+            />
+          </div>
+          <div class="col-md-5 form-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Your Email"
+              fdprocessedid="om9v4c"
+            />
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div class="col-md-12">
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Subject"
+              fdprocessedid="d4gtxq"
+            />
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div class="col-md-12">
+          <div class="form-group">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="7"
+              class="ms form-control"
+              placeholder="Message"
+            ></textarea>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div class="col-md-12">
+          <div class="form-group d-flex justify-content-center">
+            <input
+              type="submit"
+              value="Send Message"
+              class="tb btn py-3 px-5"
+              fdprocessedid="a73baa"
+            />
+          </div>
+        </div>
+      </div>
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button type="submit" disabled={state.submitting}>
         Submit
       </button>
@@ -36,3 +87,73 @@ function ConForm() {
   );
 }
 export default ConForm;
+
+// <form action="#" class=" p-md-5 contact-form" id="contact-form">
+//   <div class="row">
+//     <div class="col-md-12 d-flex justify-content-between">
+//       <div class="col-md-5 form-group">
+//         <input
+//           type="text"
+//           class="form-control"
+//           placeholder="Your Name"
+//           fdprocessedid="mpdmc9"
+//         />
+//       </div>
+//       <div class="col-md-5 form-group">
+//         <input
+//           type="text"
+//           class="form-control"
+//           placeholder="Your Email"
+//           fdprocessedid="om9v4c"
+//         />
+//       </div>
+//     </div>
+//     <br />
+//     <br />
+//     <br />
+//     <div class="col-md-12">
+//       <div class="form-group">
+//         <input
+//           type="text"
+//           class="form-control"
+//           placeholder="Subject"
+//           fdprocessedid="d4gtxq"
+//         />
+//       </div>
+//     </div>
+//     <br />
+//     <br />
+//     <br />
+//     <div class="col-md-12">
+//       <div class="form-group">
+//         <textarea
+//           name=""
+//           id=""
+//           cols="30"
+//           rows="7"
+//           class="ms form-control"
+//           placeholder="Message"
+//         ></textarea>
+//       </div>
+//     </div>
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <br />
+//     <div class="col-md-12">
+//       <div class="form-group d-flex justify-content-center">
+//         <input
+//           type="submit"
+//           value="Send Message"
+//           class="tb btn py-3 px-5"
+//           fdprocessedid="a73baa"
+//         />
+//       </div>
+//     </div>
+//   </div>
+// </form>;
